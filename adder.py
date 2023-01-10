55d8113d0c48399c7dd01152b6f6101e9d0d787d
qoft <63415260+qoft@users.noreply.github.com>
2023-01-09 21:55:05 -0500

    Update adder.py

--- adder.py
+++ adder.py
@@ -9,7 +9,7 @@ LOG_FILE = "commit_bot.log"
 
 # Commit Options
 NO_COMMIT_CHANCE = 0.1 # 10% chance of NOT committing to GitHub.
-MAX_COMMITS = 8 # Maximum number of commits that can be made.
+MAX_COMMITS = 500 # Maximum number of commits that can be made.
 
 # Cron job.
 CRON_JOB_TIME = "0 12 * * *" # Every day at 12:00 pm.
@@ -26,7 +26,7 @@ def create_commit():
     global i
     while True:
         system("git pull -X theirs")
-        for i in range(100):
+        for i in range(random.randint(1, MAX_COMMITS)):
             with open(OUTPUT_FILE, "w") as f:
                 f.write(str(datetime.now()))
             system(f"git add {OUTPUT_FILE}")
