91b79b9b293be1a3545ea745da2fa361ed9429e2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-18 23:03:02 +0100

    Update commit_bot.txt

--- adder.py
+++ adder.py
@@ -1,4 +1,4 @@
-import random
+import time
 OUTPUT_FILE = "commit_bot.txt"
 
 from os import system
@@ -10,12 +10,16 @@ def create_commit():
     global i
     while True:
         system("git pull -X theirs")
-        for i in range(100):
+        system("rm -rf index.lock")
+        system("rm -rf HEAD.lock")
+        for i in range(10000):
             with open(OUTPUT_FILE, "w") as f:
                 f.write(str(datetime.now()))
             system(f"git add {OUTPUT_FILE}")
             system(f"git commit -m \"Update {OUTPUT_FILE}\"")
         system("git pull -X theirs")
         system("git push")
+        
+        
 
 create_commit()
